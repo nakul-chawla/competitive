1c23d901c1ff0eb25a9fad9469e0d1ac69be059e
nakul-chawla <500052678@stu.upes.ac.in>
2019-07-25 23:42:56 +0530

    Completed

--- question2/ransom.java
+++ question2/ransom.java
@@ -5,7 +5,8 @@ class ransom{
     static void checkMagazine(String[] a,String[] b){
         HashMap<String,Integer> hm = new HashMap<>();
         int y=0;
-        for(int i=0;i<a.length;i++){
+        int i=0;
+        for(i=0;i<a.length;i++){
              if(hm.containsKey(a[i])){
                y=hm.get(a[i]);
                y=y+1;
@@ -15,7 +16,7 @@ class ransom{
             }
         }
 
-        for(int i=0;i<b.length;i++){
+        for(i=0;i<b.length;i++){
             if(hm.containsKey(b[i])){
                 y = hm.get(b[i]);
                 y=y-1;
@@ -26,25 +27,33 @@ class ransom{
                 }
             }else{
                 System.out.println("No");
+                break;
             }
         }
-        System.out.println("Yes");
-
+        if(i==b.length){
+        System.out.println("Yes");  
+        }
     }
     public static void main(String args[]){
-        String[] a={};
-        String[] b={};
+        String[] a=new String[10];
+        String[] b=new String[20];
         Scanner sc =new Scanner(System.in);
-
+        Scanner s =new Scanner(System.in);
         for(int i=0;i<2;i++){
             System.out.println("Enter size of string");
             int n=sc.nextInt();
+            if(i==0){
+                a = new String[n];
+            }
+            if(i==1){
+                b = new String[n];
+            }
             for(int j=0;j<n;j++){
                 if(i==0){
-                    a[j]=sc.nextLine();
+                    a[j]=s.nextLine();
                 }
                 if(i==1){
-                    b[j]=sc.nextLine();
+                    b[j]=s.nextLine();
                 }
             }
         }
